1f233d78dc7fd98d53ee083a58710363a05b32c8
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-02 12:56:02 +0530

    custom color added | layout changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ["public/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: {
+          950: '#ff0000',
+        },
+      }
+    },
   },
   plugins: [],
 }
